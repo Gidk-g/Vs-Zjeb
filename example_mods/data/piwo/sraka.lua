ea1cdbc6e0747268a958323ffd905fae5e7d5370
Gidk-g <gidkg302@gmail.com>
2023-03-03 17:57:23 +0100

    goofy ahh

---
+++ example_mods/data/piwo/sraka.lua
@@ -0,0 +1,26 @@
+local allowCountdown = false
+
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onEndSong()
+	if not allowEnd and isStoryMode then
+		startVideo('gratulacje');
+		allowEnd = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then
+		startDialogue('dialogue', 'sraka');
+	end
+end
---
+++ example_mods/stages/city.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	makeLuaSprite('bg', 'zjeb/bg', -300, -150);
+	setLuaSpriteScrollFactor('bg', 1.0, 1.0);
+	scaleObject('bg', 1.3, 1.3);
+	addLuaSprite('bg', false);
+end
